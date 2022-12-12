2c3d2d5457a193afd9524b77838f2202f442d546
zhunan123 <zhunan@bu.edu>
2022-12-12 00:57:08 -0800

    add initial UI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,34 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+
+  private static final String ANSI_RESET = "\u001B[0m";
+  private static final String ANSI_GREEN = "\u001B[32m";
+  private static final String ANSI_PURPLE = "\u001B[35m";
+  public static void startProgram() {
+    System.out.println(
+            "*****************************************************************" + "\n" +
+                    "*************" + ANSI_PURPLE + "Welcome to Zhunan's Music Manager APP" + ANSI_RESET +"***************" + "\n" +
+                    "**********" + ANSI_GREEN + "Please Enter Your User Login ID and Password" + ANSI_RESET + "***********" + "\n" +
+                    "*****************************************************************" + "\n"
+    );
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(">> Please Enter ID: ");
+    String s = null;
+    try {
+      s = br.readLine();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
+  public static void main( String[] args )
+  {
+    startProgram();
+  }
+
 }
